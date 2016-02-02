15f1eb0f0be2c67bb6fbeb9decc04fd33134a3a5
Stanislas Boyet <stanislas@boyet.me>
2016-02-02 11:26:48 +0100

    force public cors

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -4,7 +4,17 @@ module Api
 
       protect_from_forgery with: :null_session
       respond_to :json
-      
+      before_filter :set_public_cors
+
+      private
+
+      def set_public_cors
+        headers['Access-Control-Allow-Origin'] = '*'
+        headers['Access-Control-Allow-Methods'] = 'POST, PUT, DELETE, GET, OPTIONS'
+        headers['Access-Control-Request-Method'] = '*'
+        headers['Access-Control-Allow-Headers'] = 'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+      end
+
     end
   end
 end
